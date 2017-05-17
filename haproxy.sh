cb4a79ff53d63481800591c17011060930133c4e
pahli <rico.pahlisch@ottogroup.com>
2017-05-17 22:32:50 +0200

    change path

--- haproxy.sh
+++ haproxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --buffer
-./haproxy -f /tmp/haproxy.cfg -p /tmp/haproxy.pid -sf $(cat /tmp/haproxy.pid)
+./haproxy -f /conf/haproxy.cfg -p /tmp/haproxy.pid -sf $(cat /tmp/haproxy.pid)
 nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug--release-indefinite
