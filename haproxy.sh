f1277aebd21c1fe0f919c9646a24495a91cb5ebb
pahli <rico.pahlisch@googlemail.com>
2017-05-17 23:32:45 +0200

    fix

--- haproxy.sh
+++ haproxy.sh
@@ -2,4 +2,4 @@
 
 nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --buffer
 /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -D -p /var/run/haproxy.pid -sf $( cat /var/run/haproxy.pid )
-nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug--release-indefinite
+nl-qdisc-add --dev=lo --parent=1:4 --id=40: --update plug --release-indefinite
